bc2ff086382af8d3de0e7bd2c95da2d5af465e17
ShivaliBandi <shivbandi3004@gmail.com>
2021-04-25 15:27:29 +0530

    new files commited

---
+++ 1822Sign_Of_Product_OF_An_Array.cpp
@@ -0,0 +1,57 @@
+/*
+1822. Sign of the Product of an Array
+There is a function signFunc(x) that returns:
+
+1 if x is positive.
+-1 if x is negative.
+0 if x is equal to 0.
+You are given an integer array nums. Let product be the product of all values in the array nums.
+
+Return signFunc(product).
+
+ 
+
+Example 1:
+
+Input: nums = [-1,-2,-3,-4,3,2,1]
+Output: 1
+Explanation: The product of all values in the array is 144, and signFunc(144) = 1
+Example 2:
+
+Input: nums = [1,5,0,2,-3]
+Output: 0
+Explanation: The product of all values in the array is 0, and signFunc(0) = 0
+Example 3:
+
+Input: nums = [-1,1,-1,1,-1]
+Output: -1
+Explanation: The product of all values in the array is -1, and signFunc(-1) = -1
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-100 <= nums[i] <= 100
+
+
+
+*/
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+         int icnt=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<0)
+                icnt++;
+            else if(nums[i]==0)
+                return 0;
+           
+        }
+        if(icnt%2==0)
+            return 1;
+       else
+            return -1;
+       
+    }
+};
